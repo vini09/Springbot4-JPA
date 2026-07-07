f493ca48d02e2221b71e5b6cc52b23d30b3b1d1d
Vinicius Gabriel S. <vinicius78gabriel@gmail.com>
2026-07-06 21:40:32 -0300

    Inicializando novo projeto

---
+++ src/test/java/com/especializacao/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.especializacao.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

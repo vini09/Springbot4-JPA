8e005a88a4679237e922c288f33b1fad5b43572f
Vinicius Gabriel S. <vinicius78gabriel@gmail.com>
2026-07-11 12:23:24 -0300

    Banco de dados H2, test profile

--- src/main/java/com/especializacao/course/entities/User.java
+++ src/main/java/com/especializacao/course/entities/User.java
@@ -2,9 +2,21 @@ package com.especializacao.course.entities;
 
 import java.io.Serializable;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
-	private Long id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 	private String name;
 	private String email;
 	private String phone;

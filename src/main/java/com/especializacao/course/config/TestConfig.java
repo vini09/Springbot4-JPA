d5039d8f1ef04698961898ed9871a64543b4da34
Vinicius Gabriel S. <vinicius78gabriel@gmail.com>
2026-07-23 20:38:48 -0300

    Payment, one-to-one association

--- src/main/java/com/especializacao/course/config/TestConfig.java
+++ src/main/java/com/especializacao/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.especializacao.course.entities.Category;
 import com.especializacao.course.entities.Order;
 import com.especializacao.course.entities.OrderItem;
+import com.especializacao.course.entities.Payment;
 import com.especializacao.course.entities.Product;
 import com.especializacao.course.entities.User;
 import com.especializacao.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
